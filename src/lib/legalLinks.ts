87e2964a54541cf37a47503f1709a4191436bbf6
mkraja826 <37881042+mkraja826@users.noreply.github.com>
2026-08-27 09:56:08 +0530

    Use CapDent-hosted legal URLs

--- src/lib/legalLinks.ts
+++ src/lib/legalLinks.ts
@@ -1,3 +1,3 @@
-export const CAPDENT_PRIVACY_URL = "https://dms.micirql.com/privacy";
-export const CAPDENT_TERMS_URL = "https://dms.micirql.com/terms";
-export const CAPDENT_DELETE_ACCOUNT_URL = "https://dms.micirql.com/delete-account";
+export const CAPDENT_PRIVACY_URL = "https://capdent.in/privacy/";
+export const CAPDENT_TERMS_URL = "https://capdent.in/terms/";
+export const CAPDENT_DELETE_ACCOUNT_URL = "https://capdent.in/delete-account/";
